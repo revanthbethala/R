07d0344c01cf5eb64c528ab226131d86583e97a6
Revanth <bethalarevanth@gmail.com>
2025-02-27 12:19:55 +0530

    update jobs

--- frontend/src/Jobs/RecommendJobs.tsx
+++ frontend/src/Jobs/RecommendJobs.tsx
@@ -1,17 +1,9 @@
-import useGet from "@/myComponents/useGet"
-import { useUser } from "@clerk/clerk-react"
+import React from 'react'
 
 const RecommendJobs = () => {
-   const { user } = useUser()
-   const id = user?.id
-   const { data, error, isLoading } = useGet(`user/getUserByClerk/${id}`)
-   console.log(data)
-   const courseIds = data?.user?.courses
-   console.log(courseIds[0])
-
-   return (
-      <div>RecommendJobs</div>
-   )
+  return (
+    <div>RecommendJobs</div>
+  )
 }
 
 export default RecommendJobs
